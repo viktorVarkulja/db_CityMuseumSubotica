e227c9dbccb72e595b99f7fc13eb6c98311c574c
viktorVarkulja <152086556+viktorVarkulja@users.noreply.github.com>
2025-09-01 12:38:50 +0200

    Create 01_add_update_view.sql

---
+++ artifact_subsys/01_add_update_view.sql
@@ -0,0 +1,10 @@
+USE PodrskaRadaMuzeja
+GO
+CREATE VIEW pUmetanje_Azuriranje_Predmeta
+AS
+SELECT Predmet.Inv_Br# as InventarskiBroj, Predmet.Dig_Sifra as DigitalnaSifra,
+Predmet.Br_Dosije#fk as OdrzavanjeKvaliteta , Predmet.Sifra_Zbr#fk as SifraZbirke, Zbirka.Naziv_Zbr as Zbirka,
+Zbirka.ID_Odlj#fk as SifraOdeljenja, Odeljenje.Naziv_Odlj as Odeljenje, Predmet.ID_Don#fk as Donator
+FROM Predmet left join Zbirka on Predmet.Sifra_Zbr#fk=Zbirka.Sifra_Zbr#
+AND Zbirka.Naziv_Zbr=Zbirka.Naziv_Zbr
+left join Odeljenje on Zbirka.ID_Odlj#fk=Odeljenje.ID_Odlj#;
