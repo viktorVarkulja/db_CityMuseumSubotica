ed691f9016a01aa869748fe77a7d6f14e1587642
viktorVarkulja <152086556+viktorVarkulja@users.noreply.github.com>
2025-09-01 12:30:49 +0200

    Create 01_add_update_view.sql

---
+++ artifacts_subsys/01_add_update_view.sql
@@ -0,0 +1,13 @@
+USE PodrskaRadaMuzeja1
+GO
+CREATE VIEW pUmetanje_Azuriranje_Izlozbe
+AS
+SELECT Izlozbe.Sifra_Izloz# as SifraIzlozbe,
+Izlozbe.Ime_Izloz as Izlozba , Izlozbe.Ime_Sale as Sala, Izlozbe.Datum_Od_Izloz as DatumOd ,
+Izlozbe.Datum_Do_Izloz as DatumDo, Izlozbe.Ime_Zgrada#fk as Zgrada, Vodi.Sifra_Rad#fk as Autor
+
+FROM Izlozbe left join Vodi on Izlozbe.Sifra_Izloz#=Vodi.Sifra_Izloz#fk;
+
+GO
+SELECT *
+FROM pUmetanje_Azuriranje_Izlozbe
