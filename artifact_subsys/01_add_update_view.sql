cfef0ff75121a95a7fc0198035921aec426db97a
viktorVarkulja <152086556+viktorVarkulja@users.noreply.github.com>
2025-09-01 12:42:09 +0200

    Update 01_add_update_view.sql

--- artifact_subsys/01_add_update_view.sql
+++ artifact_subsys/01_add_update_view.sql
@@ -8,3 +8,7 @@ Zbirka.ID_Odlj#fk as SifraOdeljenja, Odeljenje.Naziv_Odlj as Odeljenje, Predmet.
 FROM Predmet left join Zbirka on Predmet.Sifra_Zbr#fk=Zbirka.Sifra_Zbr#
 AND Zbirka.Naziv_Zbr=Zbirka.Naziv_Zbr
 left join Odeljenje on Zbirka.ID_Odlj#fk=Odeljenje.ID_Odlj#;
+GO
+
+SELECT *
+FROM pUmetanje_Azuriranje_Predmeta
